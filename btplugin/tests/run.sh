830a0ee0df3f681c4762ecc4859ce2df5ebd3c8f
Rob Shakir <rob.shakir@bt.com>
2015-04-17 18:11:02 +0100

    Updated unit test script to allow monitoring of overall test success

--- btplugin/tests/run.sh
+++ btplugin/tests/run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+FAIL=0
 TESTDIR=/Users/rjs/Code/pyangbind/btplugin/tests/
 for i in `find $TESTDIR -mindepth 1 -maxdepth 1 -type d`; do
-    echo "TESTING $i..."
-    $i/run.py
+    #echo "TESTING $i..."
+    $i/run.py > /dev/null
     if [ $? -ne 0 ]; then
         echo "TEST FAILED $i";
+        FAIL = expr $FAIL + 1
     fi
 done
+
+if [ $FAIL -eq 0 ]; then
+    echo "RESULT: all tests passed"
+else
+    echo "RESULT: $FAIL tests failed"
+fi
+
