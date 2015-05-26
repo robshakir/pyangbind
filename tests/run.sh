00e58e1eb9ffb6c68ca440da5caca74109bc793a
Rob Shakir <rob.shakir@bt.com>
2015-05-26 20:59:48 +0100

    Minor change to tests run.sh to allow xpathhelper to be symlinked. 	* (M) tests/run.sh now creates a symlink to the xpathhelper file 		  which defines YANGPathHelper

--- tests/run.sh
+++ tests/run.sh
@@ -3,24 +3,29 @@ FAIL=0
 TESTDIR=`pwd`
 export PYANGBINDPATH=$TESTDIR/..
 export PYANGPATH=`which pyang`
+export XPATHLIBDIR=$TESTDIR/../lib/
 
 echo "RUNNING BASE"
+ln -s $XPATHLIBDIR/xpathhelper.py $TESTDIR/xpathhelper.py
 $PYANGPATH --plugindir $PYANGBINDPATH -f pybind $TESTDIR/base-test.yang -o /tmp/chkplugin.pyang >/dev/null
 if [ $? -ne 0 ]; then
     echo "RESULT: CANNOT RUN TESTS, BROKEN PLUGIN"
     exit
 fi
 rm /tmp/chkplugin.pyang
+rm $TESTDIR/xpathhelper.py
 
 if [ $# -eq 0 ]; then
     FAIL=0
     for i in `find $TESTDIR -mindepth 1 -maxdepth 1 -type d`; do
         echo "TESTING $i..."
+        ln -s $XPATHLIBDIR/xpathhelper.py $i/xpathhelper.py
         $i/run.py > /dev/null
         if [ $? -ne 0 ]; then
             echo "TEST FAILED $i";
             FAIL=$((FAIL+1))
         fi
+        rm $i/xpathhelper.py
     done
     if [ $FAIL -eq 0 ]; then
         echo "RESULT: all tests passed"
@@ -30,10 +35,12 @@ if [ $# -eq 0 ]; then
 else
     for i in "$@"; do
         echo "TESTING $i..."
+        ln -s $XPATHLIBDIR/xpathhelper.py $i/xpathhelper.py
         $i/run.py > /dev/null
         if [ $? -ne 0 ]; then
             echo "TEST FAILED $i";
         fi
+        rm $i/xpathhelper.py
     done
 fi
 
