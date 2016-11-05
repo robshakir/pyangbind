5e3e2472f5ee8a7fe8ec46083591725ee3b6471e
Tim Martin <tim@asymptotic.co.uk>
2016-11-05 15:11:43 +0000

    Fixed UTs that use unittest.main not supporting the -k argument

--- tests/misc/run.py
+++ tests/misc/run.py
@@ -102,7 +102,10 @@ class PyangbindMiscTests(unittest.TestCase):
 
 if __name__ == '__main__':
   setup_test()
-  T = unittest.main(exit=False)
+  args = sys.argv
+  if '-k' in args:
+    args.remove('-k')
+  T = unittest.main(exit=False, argv=args)
   if len(T.result.errors) or len(T.result.failures):
     exitcode = 127
   else:
