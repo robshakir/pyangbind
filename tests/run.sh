dbd3dcb3d16b981a909fe6fcb6290c97af18e580
David Barroso <dbarrosop@dravetech.com>
2017-08-27 19:12:35 +0200

    enable py3 testing

--- tests/run.sh
+++ tests/run.sh
@@ -11,36 +11,7 @@ function runtests {
     echo -n "Testing with "
     $PYTHON_INTERPRETER --version
 
-    # Build a virtual environment to do the tests in
-    cd $TESTDIR/..
-    rm -rf $TESTDIR/pyvirtualenv $TESTDIR/../dist $TESTDIR/../build $TESTDIR/../pyangbind.egg-info > /dev/null
-
-    echo "RUNNING PACKAGING..."
-    $PYTHON_INTERPRETER setup.py bdist_wheel sdist > /dev/null
-    if [ $? -ne 0 ]; then
-        echo "RESULT: CANNOT RUN TESTS, BROKEN PACKAGING"
-        exit
-    fi
-
-    echo "CREATING VIRTUALENV..."
-    virtualenv -p $PYTHON_INTERPRETER $TESTDIR/pyvirtualenv > /dev/null
-    if [ $? -ne 0 ]; then
-        echo "RESULT: CANNOT RUN TESTS, BROKEN VIRTUALENV"
-        exit
-    fi
-
-    source $TESTDIR/pyvirtualenv/bin/activate
-
-    echo "INSTALLING MODULE..."
-    $TESTDIR/pyvirtualenv/bin/pip install -r $TESTDIR/../requirements.txt > /dev/null
-    $TESTDIR/pyvirtualenv/bin/pip install -r $TESTDIR/../requirements.DEVELOPER.txt > /dev/null
-    $TESTDIR/pyvirtualenv/bin/pip install $TESTDIR/../dist/*.whl > /dev/null
-    if [ $? -ne 0 ]; then
-        echo "RESULT: CANNOT RUN TESTS, BROKEN INSTALL"
-        exit 127
-    fi
-
-    export PATH_TO_PYBIND_TEST_PYTHON="$TESTDIR/pyvirtualenv/bin/python"
+    export PATH_TO_PYBIND_TEST_PYTHON="python"
     FAIL=0
 
     for TEST in $TESTDIR/yang_tests.sh $TESTDIR/xpath/xpath_tests.sh $TESTDIR/serialise/serialise_tests.sh $TESTDIR/integration/integration_tests.sh; do
@@ -60,5 +31,4 @@ function runtests {
     fi
 }
 
-runtests "python2.7"
-runtests "python3.6"
+runtests "python"
