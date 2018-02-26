e2e02fcc68e61f14828140499d583d9c22b258cc
Joey Wilhelm <jwilhelm@godaddy.com>
2018-02-26 14:22:56 -0700

    Create a base class for unit tests
    This is a subclass of unittest.TestCase (or unittest2.TestCase for
    Python 2). The main piece of functionality this provides is creating a
    `self.bindings`, which behaves exactly as though you had imported a
    Python module named `bindings`. The reason for doing this instead of
    trying to simply import the bindings file is that this eliminates any
    potential issues which could arise from waiting for a disk write and
    then a disk read. This code never even touches the disk, and is stored
    entirely in memory.

---
+++ tests/base.py
@@ -0,0 +1,38 @@
+import distutils
+import inspect
+import os.path
+import subprocess
+import types
+try:
+  import unittest2 as unittest
+except ImportError:
+  import unittest
+
+
+class PyangBindTestCase(unittest.TestCase):
+  yang_files = None
+
+  @classmethod
+  def setUpClass(cls):
+    if cls.yang_files is None:
+      raise ValueError("cls.yang_files must be set")
+    pyang_path = distutils.spawn.find_executable('pyang')
+    if not pyang_path:
+      raise RuntimeError("Could not locate `pyang` executable.")
+    base_dir, _ = os.path.abspath(__file__).split('{}tests{}'.format(os.path.sep, os.path.sep))
+    test_path = os.path.dirname(inspect.getfile(cls))
+    yang_files = [os.path.join(test_path, filename) for filename in cls.yang_files]
+    plugin_dir = os.path.join(base_dir, 'pyangbind', 'plugin')
+
+    pyang_cmd = "{} --plugindir {} -f pybind -p {} --use-extmethods --use-xpathhelper {}".format(
+      pyang_path, plugin_dir, test_path, ' '.join(yang_files)
+    )
+    bindings_code = subprocess.check_output(
+      pyang_cmd, shell=True, stderr=subprocess.STDOUT, env={'PYTHONPATH': base_dir}
+    )
+    cls.bindings = types.ModuleType('bindings')
+    exec(bindings_code, cls.bindings.__dict__)
+
+  @classmethod
+  def tearDownClass(cls):
+    del cls.bindings
