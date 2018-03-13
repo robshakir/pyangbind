e45b32286340ac8fa08e1164a0076bb9aeb79f57
Joey Wilhelm <jwilhelm@godaddy.com>
2018-03-13 08:33:05 -0700

    Add support for "split_class_dir" and "module_name"

--- tests/base.py
+++ tests/base.py
@@ -1,7 +1,10 @@
 import distutils
+import importlib
 import inspect
 import os.path
+import shutil
 import subprocess
+import sys
 import types
 try:
   import unittest2 as unittest
@@ -12,6 +15,8 @@ except ImportError:
 class PyangBindTestCase(unittest.TestCase):
   yang_files = None
   pyang_flags = []
+  split_class_dir = False
+  module_name = 'bindings'
 
   @classmethod
   def setUpClass(cls):
@@ -25,6 +30,11 @@ class PyangBindTestCase(unittest.TestCase):
     yang_files = [os.path.join(test_path, filename) for filename in cls.yang_files]
     plugin_dir = os.path.join(base_dir, 'pyangbind', 'plugin')
 
+    pyang_flags = cls.pyang_flags
+    if cls.split_class_dir is True:
+      cls._pyang_generated_class_dir = os.path.join(test_path, cls.module_name)
+      pyang_flags.append('--split-class-dir {}'.format(cls._pyang_generated_class_dir))
+
     pyang_cmd = "{pyang} --plugindir {plugins} -f pybind -p {test_path} {flags} {yang_files}".format(
       pyang=pyang_path,
       plugins=plugin_dir,
@@ -35,9 +45,16 @@ class PyangBindTestCase(unittest.TestCase):
     bindings_code = subprocess.check_output(
       pyang_cmd, shell=True, stderr=subprocess.STDOUT, env={'PYTHONPATH': base_dir}
     )
-    cls.bindings = types.ModuleType('bindings')
-    exec(bindings_code, cls.bindings.__dict__)
+    if not cls.split_class_dir:
+      module = types.ModuleType(cls.module_name)
+      exec(bindings_code, module.__dict__)
+    else:
+      sys.path.append(test_path)
+      module = importlib.import_module(cls.module_name)
+    setattr(cls, cls.module_name, module)
 
   @classmethod
   def tearDownClass(cls):
-    del cls.bindings
+    delattr(cls, cls.module_name)
+    if cls.split_class_dir:
+      shutil.rmtree(cls._pyang_generated_class_dir)
