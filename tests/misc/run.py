808f77448f52b288bb3b98a66c69a66917f0e9af
Joey Wilhelm <jwilhelm@godaddy.com>
2018-03-13 08:35:52 -0700

    Convert the misc tests to use unittest

--- tests/misc/run.py
+++ tests/misc/run.py
@@ -1,66 +1,20 @@
 #!/usr/bin/env python
+from __future__ import unicode_literals
 
-import os
-import sys
-import getopt
 import unittest
+
 from pyangbind.lib.xpathhelper import YANGPathHelper
-
-TESTNAME = "misc"
-
-
-# generate bindings in this folder
-def setup_test():
-  try:
-    opts, args = getopt.getopt(sys.argv[1:], "k", ["keepfiles"])
-  except getopt.GetoptError:
-    sys.exit(127)
-
-  global this_dir
-
-  pythonpath = os.environ.get("PATH_TO_PYBIND_TEST_PYTHON") if \
-                os.environ.get('PATH_TO_PYBIND_TEST_PYTHON') is not None \
-                  else sys.executable
-  pyangpath = os.environ.get('PYANGPATH') if \
-                os.environ.get('PYANGPATH') is not None else False
-  pyangbindpath = os.environ.get('PYANGBINDPATH') if \
-                os.environ.get('PYANGBINDPATH') is not None else False
-  assert pyangpath is not False, "could not find path to pyang"
-  assert pyangbindpath is not False, "could not resolve pyangbind directory"
-
-  this_dir = os.path.dirname(os.path.realpath(__file__))
-
-  cmd = "%s " % pythonpath
-  cmd += "%s --plugindir %s/pyangbind/plugin" % (pyangpath, pyangbindpath)
-  cmd += " -f pybind"
-  cmd += " -p %s" % this_dir
-  cmd += " --use-extmethods"
-  cmd += " --split-class-dir %s/bindings" % this_dir
-  cmd += " --use-xpathhelper"
-  cmd += " %s/%s.yang" % (this_dir, TESTNAME)
-  os.system(cmd)
+from tests.base import PyangBindTestCase
 
 
-def teardown_test():
-  global this_dir
+class MiscTests(PyangBindTestCase):
+  yang_files = ['misc.yang']
+  split_class_dir = True
+  pyang_flags = ['--use-extmethods', '--use-xpathhelper']
 
-  os.system("/bin/rm -rf %s/bindings" % this_dir)
-
-
-class PyangbindMiscTests(unittest.TestCase):
-
-  def __init__(self, *args, **kwargs):
-    unittest.TestCase.__init__(self, *args, **kwargs)
-
-    self.ph = YANGPathHelper()
-
-    err = None
-    try:
-      import bindings
-    except ImportError as e:
-      err = e
-    self.assertIs(err, None)
-    self.instance = bindings.misc(path_helper=self.ph)
+  def setUp(self):
+    self.path_helper = YANGPathHelper()
+    self.instance = self.bindings.misc(path_helper=self.path_helper)
 
   # Check that we can ingest an OpenConfig style list entry
   # with a leafref to the key
@@ -70,8 +24,8 @@ class PyangbindMiscTests(unittest.TestCase):
     a.foo = "stringval"
 
     self.instance.a.append(a)
-    self.assertEqual(unicode(self.instance.a["stringval"].foo), u"stringval")
-    self.assertEqual(self.instance.a["stringval"].config.foo, u"stringval")
+    self.assertEqual(unicode(self.instance.a["stringval"].foo), "stringval")
+    self.assertEqual(self.instance.a["stringval"].config.foo, "stringval")
 
   def test_002_checklistkeytype(self):
     import bindings.b as miscb
@@ -80,7 +34,7 @@ class PyangbindMiscTests(unittest.TestCase):
     b.bar = "stringvaltwo"
 
     self.instance.b.append(b)
-    self.assertEqual(type(self.instance.b.keys()[0]), unicode)
+    self.assertIsInstance(self.instance.b.keys()[0], unicode)
 
   def test_003_checklistkeytype(self):
     import bindings.c as miscc
@@ -88,23 +42,8 @@ class PyangbindMiscTests(unittest.TestCase):
     c.one = 42
 
     self.instance.c.append(c)
-    self.assertEqual(type(self.instance.c.keys()[0]), int)
+    self.assertIsInstance(self.instance.c.keys()[0], int)
 
 
 if __name__ == '__main__':
-  keepfiles = False
-  args = sys.argv
-  if '-k' in args:
-    args.remove('-k')
-    keepfiles = True
-
-  setup_test()
-  T = unittest.main(exit=False)
-  if len(T.result.errors) or len(T.result.failures):
-    exitcode = 127
-  else:
-    exitcode = 0
-
-  if keepfiles is False:
-    teardown_test()
-  sys.exit(exitcode)
+  unittest.main(exit=False)
