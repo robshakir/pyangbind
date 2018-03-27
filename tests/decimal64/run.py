668e0df7fd62d4b0a034b0d574fa2a143cde6d29
Joey Wilhelm <tarkatronic@gmail.com>
2018-03-26 23:37:54 -0700

    More unittest conversions (#165)
    * Convert the decimal64 tests to use unittest
    
    * Convert the enumeration tests to use unittest
    
    * Convert the extensions tests to use unittest
    
    * Convert the include-import tests to use unittest
    
    * Flush and sync the files to ensure they are written to disk
    
    * Add support for "split_class_dir" and "module_name"
    
    * Convert the misc tests to use unittest

--- tests/decimal64/run.py
+++ tests/decimal64/run.py
@@ -1,83 +1,54 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import getopt
+from decimal import Decimal
+import unittest
 
-TESTNAME = "decimal"
+from tests.base import PyangBindTestCase
 
 
-# generate bindings in this folder
-def main():
-  try:
-    opts, args = getopt.getopt(sys.argv[1:], "k", ["keepfiles"])
-  except getopt.GetoptError as e:
-    print(str(e))
-    sys.exit(127)
+class DecimalTests(PyangBindTestCase):
+  yang_files = ['decimal.yang']
 
-  k = False
-  for o, a in opts:
-    if o in ["-k", "--keepfiles"]:
-      k = True
+  def setUp(self):
+    self.decimal_obj = self.bindings.decimal_()
 
-  pythonpath = os.environ.get("PATH_TO_PYBIND_TEST_PYTHON") if \
-                os.environ.get('PATH_TO_PYBIND_TEST_PYTHON') is not None \
-                  else sys.executable
-  pyangpath = os.environ.get('PYANGPATH') if \
-                os.environ.get('PYANGPATH') is not None else False
-  pyangbindpath = os.environ.get('PYANGBINDPATH') if \
-                os.environ.get('PYANGBINDPATH') is not None else False
-  assert pyangpath is not False, "could not find path to pyang"
-  assert pyangbindpath is not False, "could not resolve pyangbind directory"
+  def test_container_has_all_leafs(self):
+    for leaf in ['d1', 'd2', 'd3']:
+      with self.subTest(leaf=leaf):
+        self.assertTrue(hasattr(self.decimal_obj.container, leaf), "Container missing attribute - %s" % leaf)
 
-  this_dir = os.path.dirname(os.path.realpath(__file__))
+  def test_decimal_precision(self):
+    self.decimal_obj.container.d1 = 42.0
+    self.assertEqual(str(self.decimal_obj.container.d1), "42.00",
+      "Precision for d1 was incorrect %s" % self.decimal_obj.container.d1)
 
-  cmd = "%s " % pythonpath
-  cmd += "%s --plugindir %s/pyangbind/plugin" % (pyangpath, pyangbindpath)
-  cmd += " -f pybind -o %s/bindings.py" % this_dir
-  cmd += " -p %s" % this_dir
-  cmd += " %s/%s.yang" % (this_dir, TESTNAME)
-  os.system(cmd)
+  def test_decimal_rounding(self):
+    self.decimal_obj.container.d2 = 42.0009
+    self.assertEqual(self.decimal_obj.container.d2, Decimal("42.001"),
+      "Precision did not result in correct rounding (%s)" % self.decimal_obj.container.d2)
 
-  from bindings import decimal_ as d
-  from decimal import Decimal
+  def test_decimal_default_with_extra_precision(self):
+    self.assertEqual(self.decimal_obj.container.d2._default, Decimal("42.000"),
+      "Default was wrong for d2 (%s)" % self.decimal_obj.container.d2._default)
 
-  q = d()
+  def test_decimal_default_with_less_precision(self):
+    self.assertEqual(self.decimal_obj.container.d3._default, Decimal("1"),
+      "Default was set wrong for d3 (%s)" % self.decimal_obj.container.d3._default)
 
-  for i in ["d1", "d2", "d3"]:
-    assert hasattr(q.container, i), "container missing attribute - %s" % i
-
-  q.container.d1 = 42.0
-  assert len(str(q.container.d1).split(".")[1]) == 2, \
-      "precision for d1 was incorrect %s" % q.container.d1
-  q.container.d2 = 42.0009
-  assert q.container.d2 == Decimal("42.001"), \
-      "precision did not result in correct rounding (%s)" \
-          % q.container.d2
-  assert q.container.d2._default == Decimal("42.000"), \
-      "default was set wrong for d2 (%s)" \
-          % q.container.d2._default
-  assert q.container.d3._default == Decimal("1"), \
-      "default was set wrong for d3 (%s)" \
-          % q.container.d3._default
-
-  for i in [(-452.6729, False), (-444.44, True), (-443.22, False),
-            (-330, True), (-222.21, False), (111.2, False), (111.1, True),
-            (446.56, True), (555.55559282, False)]:
-    passed = False
-    try:
-      q.container.dec64LeafWithRange = i[0]
-      passed = True
-    except ValueError:
-      pass
-    assert passed == i[1], \
-        "decimal64 leaf with range was not correctly set (%f -> %s != %s)" \
-            % (i[0], passed, i[1])
-
-  if not k:
-    os.system("/bin/rm %s/bindings.py" % this_dir)
-    os.system("/bin/rm %s/bindings.pyc" % this_dir)
+  def test_various_values_with_complex_range(self):
+    for value in [(-452.6729, False), (-444.44, True), (-443.22, False),
+                  (-330, True), (-222.21, False), (111.2, False), (111.1, True),
+                  (446.56, True), (555.55559282, False)]:
+      with self.subTest(value=value):
+        allowed = True
+        try:
+          self.decimal_obj.container.dec64LeafWithRange = value[0]
+        except ValueError:
+          allowed = False
+        self.assertEqual(allowed, value[1],
+          "Decimal64 leaf with range was not correctly set (%f -> %s != %s)" %
+          (value[0], allowed, value[1]))
 
 
 if __name__ == '__main__':
-  main()
+  unittest.main()
