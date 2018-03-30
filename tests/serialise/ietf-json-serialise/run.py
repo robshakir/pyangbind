ef647ff92895fa2cd583b594810110241263d821
Joey Wilhelm <jwilhelm@godaddy.com>
2018-03-30 14:42:20 -0400

    Convert ietf-json-serialise tests to unittest

--- tests/serialise/ietf-json-serialise/run.py
+++ tests/serialise/ietf-json-serialise/run.py
@@ -1,125 +1,85 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import getopt
 import json
-import difflib
-from pyangbind.lib.serialise import pybindIETFJSONEncoder
+import os.path
+import unittest
 from decimal import Decimal
 
-TESTNAME = "ietf-json-serialise"
+from bitarray import bitarray
+
+from pyangbind.lib.serialise import pybindIETFJSONEncoder
+from pyangbind.lib.xpathhelper import YANGPathHelper
+from tests.base import PyangBindTestCase
 
 
-# generate bindings in this folder
-def main():
-  try:
-    opts, args = getopt.getopt(sys.argv[1:], "k", ["keepfiles"])
-  except getopt.GetoptError as e:
-    print str(e)
-    sys.exit(127)
+class IETFJSONDeserialiseTests(PyangBindTestCase):
+  yang_files = ['ietf-json-serialise.yang', 'augment.yang']
+  maxDiff = None
 
-  k = False
-  for o, a in opts:
-    if o in ["-k", "--keepfiles"]:
-      k = True
+  def setUp(self):
+    self.yang_helper = YANGPathHelper()
+    self.serialise_obj = self.bindings.ietf_json_serialise(path_helper=self.yang_helper)
 
-  pythonpath = os.environ.get("PATH_TO_PYBIND_TEST_PYTHON") if \
-                os.environ.get('PATH_TO_PYBIND_TEST_PYTHON') is not None \
-                  else sys.executable
-  pyangpath = os.environ.get('PYANGPATH') if \
-                os.environ.get('PYANGPATH') is not None else False
-  pyangbindpath = os.environ.get('PYANGBINDPATH') if \
-                os.environ.get('PYANGBINDPATH') is not None else False
-  assert pyangpath is not False, "could not find path to pyang"
-  assert pyangbindpath is not False, "could not resolve pyangbind directory"
+  def test_serialise_single_object(self):
+    self.serialise_obj.c1.l1.add(1)
+    self.serialise_obj.c1.l1[1].string = "bear"
+    actual_json = json.loads(json.dumps(
+      pybindIETFJSONEncoder.generate_element(self.serialise_obj.c1.l1[1].string, flt=True),
+      cls=pybindIETFJSONEncoder,
+      indent=4
+    ))
+    self.assertEqual(actual_json, "bear", "Single element JSON did not match the expected output")
 
-  this_dir = os.path.dirname(os.path.realpath(__file__))
+  def test_serialise_full_container(self):
+    self.serialise_obj.c1.l1.add(1)
+    for signed in ["int", "uint"]:
+      for size in [8, 16, 32, 64]:
+        name = "%s%s" % (signed, size)
+        setter = getattr(self.serialise_obj.c1.l1[1], "_set_%s" % name)
+        setter(1)
+    self.serialise_obj.c1.l1[1].restricted_integer = 6
+    self.serialise_obj.c1.l1[1].string = "bear"
+    self.serialise_obj.c1.l1[1].restricted_string = "aardvark"
+    self.serialise_obj.c1.l1[1].union = 16
+    self.serialise_obj.c1.l1[1].union_list.append(16)
+    self.serialise_obj.c1.l1[1].union_list.append("chicken")
+    self.serialise_obj.c1.l1[1].empty = True
 
-  cmd = "%s " % pythonpath
-  cmd += "%s --plugindir %s/pyangbind/plugin" % (pyangpath, pyangbindpath)
-  cmd += " -f pybind -o %s/bindings.py" % this_dir
-  cmd += " -p %s" % this_dir
-  cmd += " %s/%s.yang" % (this_dir, TESTNAME)
-  cmd += " %s/augment.yang" % this_dir
-  os.system(cmd)
+    self.serialise_obj.c1.t1.add(16)
+    self.serialise_obj.c1.t1.add(32)
+    self.serialise_obj.c1.l1[1].leafref = 16
 
-  from bindings import ietf_json_serialise
-  from bitarray import bitarray
-  from pyangbind.lib.xpathhelper import YANGPathHelper
+    self.serialise_obj.c1.l1[1].binary = bitarray("010101")
+    self.serialise_obj.c1.l1[1].boolean = True
+    self.serialise_obj.c1.l1[1].enumeration = "one"
+    self.serialise_obj.c1.l1[1].identityref = "idone"
+    self.serialise_obj.c1.l1[1].remote_identityref = "stilton"
+    self.serialise_obj.c1.l1[1].typedef_one = "test"
+    self.serialise_obj.c1.l1[1].typedef_two = 8
+    self.serialise_obj.c1.l1[1].one_leaf = "hi"
+    self.serialise_obj.c1.l1[1].uint64type = 2**22
+    self.serialise_obj.c1.l1[1].typedef_decimal = 32.29
+    self.serialise_obj.c1.l1[1].typedef_decimalrange = Decimal("33.44")
+    self.serialise_obj.c1.l1[1].range_decimal = Decimal("4.44443322")
+    for i in range(1, 5):
+      self.serialise_obj.c1.l1[1].ll.append(unicode(i))
+    self.serialise_obj.c1.l1[1].next_hop.append("DROP")
+    self.serialise_obj.c1.l1[1].next_hop.append("192.0.2.1")
+    self.serialise_obj.c1.l1[1].next_hop.append("TEST")
+    self.serialise_obj.augtarget.augleaf = "teststring"
+    self.serialise_obj.c1.l1[1].decleaf = Decimal('42.4422')
+    for i in range(1, 10):
+      self.serialise_obj.c1.l2.add(i)
 
-  y = YANGPathHelper()
-  js = ietf_json_serialise(path_helper=y)
+    pybind_json = json.loads(json.dumps(
+      pybindIETFJSONEncoder.generate_element(self.serialise_obj, flt=True),
+      cls=pybindIETFJSONEncoder,
+      indent=4
+    ))
+    external_json = json.load(open(os.path.join(os.path.dirname(__file__), "json", "obj.json"), 'r'))
 
-  js.c1.l1.add(1)
-  for s in ["int", "uint"]:
-    for l in [8, 16, 32, 64]:
-      name = "%s%s" % (s, l)
-      x = getattr(js.c1.l1[1], "_set_%s" % name)
-      x(1)
-  js.c1.l1[1].restricted_integer = 6
-  js.c1.l1[1].string = "bear"
-  js.c1.l1[1].restricted_string = "aardvark"
-  js.c1.l1[1].union = 16
-  js.c1.l1[1].union_list.append(16)
-  js.c1.l1[1].union_list.append("chicken")
-  js.c1.l1[1].empty = True
-
-  js.c1.t1.add(16)
-  js.c1.t1.add(32)
-  js.c1.l1[1].leafref = 16
-
-  js.c1.l1[1].binary = bitarray("010101")
-  js.c1.l1[1].boolean = True
-  js.c1.l1[1].enumeration = "one"
-  js.c1.l1[1].identityref = "idone"
-  js.c1.l1[1].remote_identityref = "stilton"
-  js.c1.l1[1].typedef_one = "test"
-  js.c1.l1[1].typedef_two = 8
-  js.c1.l1[1].one_leaf = "hi"
-  js.c1.l1[1].uint64type = 2**22
-  js.c1.l1[1].typedef_decimal = 32.29
-  js.c1.l1[1].typedef_decimalrange = Decimal("33.44")
-  js.c1.l1[1].range_decimal = Decimal("4.44443322")
-
-  for i in range(1, 5):
-    js.c1.l1[1].ll.append(unicode(i))
-  js.c1.l1[1].next_hop.append("DROP")
-  js.c1.l1[1].next_hop.append("192.0.2.1")
-  js.c1.l1[1].next_hop.append("TEST")
-
-  js.augtarget.augleaf = "teststring"
-
-  js.c1.l1[1].decleaf = Decimal('42.4422')
-
-  for i in range(1, 10):
-    js.c1.l2.add(i)
-
-  pybind_json = json.loads(json.dumps(
-                  pybindIETFJSONEncoder.generate_element(js, flt=True),
-                  cls=pybindIETFJSONEncoder, indent=4))
-  external_json = json.load(
-                      open(os.path.join(this_dir, "json", "obj.json"), 'r'))
-
-  assert pybind_json == external_json, "JSON did not match the expected output, " + \
-    "diff: %s\n" % diff(pybind_json, external_json)
-
-  # Check that we can serialise a single element on its own.
-  pybind_json = json.loads(json.dumps(
-                  pybindIETFJSONEncoder.generate_element(js.c1.l1[1].string, flt=True),
-                  cls=pybindIETFJSONEncoder, indent=4))
-  assert pybind_json == "bear", "Single element JSON did not match the expected output"
-
-  if not k:
-    os.system("/bin/rm %s/bindings.py" % this_dir)
-    os.system("/bin/rm %s/bindings.pyc" % this_dir)
-
-
-def diff(a, b):
-    ja = json.dumps(a, indent=4, sort_keys=True).split("\n")
-    jb = json.dumps(b, indent=4, sort_keys=True).split("\n")
-    return '\n'.join(difflib.unified_diff(ja, jb))
+    self.assertEqual(pybind_json, external_json, "JSON did not match the expected output.")
 
 
 if __name__ == '__main__':
-  main()
+  unittest.main()
