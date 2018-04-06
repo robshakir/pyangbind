687e5b4681f08be43637f09caae99f0aade32c37
Joey Wilhelm <tarkatronic@gmail.com>
2018-04-06 08:38:17 -0700

    Half of the serialise tests converted to unittest (#168)
    * Convert the json-serialise tests to use unittest
    
    * Convert json-deserialise tests to unittest
    
    * Convert ietf-json-serialise tests to unittest
    
    * Fix a couple Py3 compatibility issues.

--- tests/serialise/json-deserialise/run.py
+++ tests/serialise/json-deserialise/run.py
@@ -1,150 +1,144 @@
 #!/usr/bin/env python
+from __future__ import unicode_literals
 
-import os
-import sys
-import getopt
 import json
-import pyangbind.lib.serialise as pbS
-from pyangbind.lib.serialise import pybindJSONDecoder
-import pyangbind.lib.pybindJSON as pbJ
+import os.path
+import unittest
 from bitarray import bitarray
 from decimal import Decimal
+
+import pyangbind.lib.pybindJSON as pbJ
+import pyangbind.lib.serialise as pbS
+from pyangbind.lib.serialise import pybindJSONDecoder
 from pyangbind.lib.xpathhelper import YANGPathHelper
 
-TESTNAME = "json-deserialise"
+from tests.base import PyangBindTestCase
 
 
-# generate bindings in this folder
-def main():
-  try:
-    opts, args = getopt.getopt(sys.argv[1:], "k", ["keepfiles"])
-  except getopt.GetoptError as e:
-    print str(e)
-    sys.exit(127)
+class JSONDeserialiseTests(PyangBindTestCase):
+  yang_files = ['json-deserialise.yang']
 
-  k = False
-  for o, a in opts:
-    if o in ["-k", "--keepfiles"]:
-      k = True
+  def setUp(self):
+    self.yang_helper = YANGPathHelper()
+    self.deserialise_obj = self.bindings.json_deserialise(path_helper=self.yang_helper)
 
-  pythonpath = os.environ.get("PATH_TO_PYBIND_TEST_PYTHON") if \
-                os.environ.get('PATH_TO_PYBIND_TEST_PYTHON') is not None \
-                  else sys.executable
-  pyangpath = os.environ.get('PYANGPATH') if \
-                os.environ.get('PYANGPATH') is not None else False
-  pyangbindpath = os.environ.get('PYANGBINDPATH') if \
-                os.environ.get('PYANGBINDPATH') is not None else False
-  assert pyangpath is not False, "could not find path to pyang"
-  assert pyangbindpath is not False, "could not resolve pyangbind directory"
+  def test_load_full_object(self):
+    expected_json = {'load-list': {'1': {'index': 1, 'value': 'one'},
+                                   '3': {'index': 3, 'value': 'three'},
+                                   '2': {'index': 2, 'value': 'two'}}}
+    # For developers looking for examples, note that the arguments here are:
+    #   - the file that we're trying to read
+    #   - the bindings module that we generated
+    #   - the name of the class within that module
+    #   kwargs (path_helper, overwrite, etc.)
+    actual_json = pbJ.load(
+      os.path.join(os.path.dirname(__file__), "json", "list.json"),
+      self.bindings,
+      "json_deserialise",
+      path_helper=self.yang_helper
+    ).get(filter=True)
 
-  this_dir = os.path.dirname(os.path.realpath(__file__))
+    self.assertEqual(actual_json, expected_json, "Whole object load did not return the correct list.")
 
-  cmd = "%s " % pythonpath
-  cmd += "%s --plugindir %s/pyangbind/plugin" % (pyangpath, pyangbindpath)
-  cmd += " -f pybind -o %s/bindings.py" % this_dir
-  cmd += " -p %s" % this_dir
-  cmd += " %s/%s.yang" % (this_dir, TESTNAME)
-  os.system(cmd)
+  def test_load_into_existing_object(self):
+    pbS.pybindJSONDecoder.load_json(
+      json.load(open(os.path.join(os.path.dirname(__file__), "json", "list-items.json"), 'r')),
+      None, None, obj=self.deserialise_obj
+    )
+    expected_json = {'load-list': {'5': {'index': 5, 'value': 'five'},
+                                   '4': {'index': 4, 'value': 'four'}}}
+    actual_json = self.deserialise_obj.get(filter=True)
+    self.assertEqual(actual_json, expected_json, "Existing object load did not return the correct list.")
 
-  import bindings
-
-  y = YANGPathHelper()
-
-  # For developers looking for examples, note that the arguments here are:
-  #   - the file that we're trying to read
-  #   - the bindings module that we generated
-  #   - the name of the class within that module
-  #   kwargs (path_helper, overwrite, etc.)
-  whole_obj = pbJ.load(os.path.join(this_dir, "json", "list.json"), bindings,
-                "json_deserialise", path_helper=y)
-
-  expected_whole_obj = {'load-list': {u'1': {'index': 1, 'value': u'one'},
-                          u'3': {'index': 3, 'value': u'three'},
-                              u'2': {'index': 2, 'value': u'two'}}}
-  assert expected_whole_obj == whole_obj.get(filter=True), \
-                      "Whole object load did not return the correct list"
-  del whole_obj
-
-  js = bindings.json_deserialise(path_helper=y)
-  # Load into an existing class
-  pbS.pybindJSONDecoder.load_json(json.load(
-            open(os.path.join(this_dir, "json", "list-items.json"), 'r')),
-              None, None, obj=js)
-  expected_get = {'load-list': {u'5': {'index': 5, 'value': u'five'},
-                    u'4': {'index': 4, 'value': u'four'}}}
-  assert expected_get == js.get(filter=True), \
-            "Existing object load did not return the correct list"
-  del expected_get
-
-  all_items = pbJ.load(os.path.join(this_dir, "json", "alltypes.json"),
-                bindings, "json_deserialise", path_helper=y)
-  expected_get = {
+  def test_all_the_types(self):
+    expected_json = {
       'c1': {
         'l1': {
-          u'1': {
-            'one-leaf': u'hi',
-            'typedef-one': u'test',
+          '1': {
+            'one-leaf': 'hi',
+            'typedef-one': 'test',
             'boolean': True,
             'binary': bitarray('111111'),
-            'union': u'16',
-            'identityref': u'idone',
-            'enumeration': u'one',
+            'union': '16',
+            'identityref': 'idone',
+            'enumeration': 'one',
             'k1': 1,
             'uint16': 1,
-            'union-list': [16, u'chicken'],
+            'union-list': [16, 'chicken'],
             'uint32': 1,
             'int32': 1,
             'int16': 1,
-            'string': u'bear',
+            'string': 'bear',
             'decimal': Decimal('42.42'),
             'typedef-two': 8,
             'uint8': 1,
             'restricted-integer': 6,
-            'leafref': u'16',
+            'leafref': '16',
             'int8': 1,
             'uint64': 1,
             'int64': 1,
-            'restricted-string': u'aardvark'
+            'restricted-string': 'aardvark'
           }
         },
         't1': {
-          u'32': {'target': u'32'},
-          u'16': {'target': u'16'}
+          '32': {'target': '32'},
+          '16': {'target': '16'}
         }
       }
     }
+    actual_json = pbJ.load(
+      os.path.join(os.path.dirname(__file__), "json", "alltypes.json"),
+      self.bindings,
+      "json_deserialise",
+      path_helper=self.yang_helper
+    ).get(filter=True)
 
-  assert all_items.get(filter=True) == expected_get, \
-            "Load of object with all items not correct"
+    self.assertEqual(actual_json, expected_json, "Load of object with all items not correct.")
 
-  del js
-  js = pbJ.load(os.path.join(this_dir, "json", "orderedlist-order.json"),
-                bindings, "json_deserialise", path_helper=y)
-  assert js.ordered.keys() == ["two", "one"], \
-            "Did not correctly load a user ordered list"
+  def test_load_user_ordered_list(self):
+    actual_json = pbJ.load(
+      os.path.join(os.path.dirname(__file__), "json", "orderedlist-order.json"),
+      self.bindings,
+      "json_deserialise",
+      path_helper=self.yang_helper
+    )
+    self.assertEqual(actual_json.ordered.keys(), ["two", "one"], "Did not correctly load a user ordered list.")
 
-  del js
-  js = pbJ.load(os.path.join(this_dir, "json", "orderedlist-no-order.json"),
-                bindings, "json_deserialise", path_helper=y)
-  assert js.ordered.keys() == ["two", "one"], \
-        "Did not correctly follow ordering in JSON file"
+  def test_load_json_ordered_list(self):
+    actual_json = pbJ.load(
+      os.path.join(os.path.dirname(__file__), "json", "orderedlist-no-order.json"),
+      self.bindings,
+      "json_deserialise",
+      path_helper=self.yang_helper
+    )
+    self.assertEqual(actual_json.ordered.keys(), ["two", "one"], "Did not correctly follow ordering in JSON file.")
 
-  pth = os.path.join(this_dir, "json", "nonexist.json")
-  for i in [True, False]:
-    success = True
+  def test_skip_unknown_keys(self):
+    allowed = True
     try:
-      pybindJSONDecoder.load_ietf_json(json.load(open(pth, 'r')),
-        bindings, "json_deserialise", skip_unknown=i)
+      pybindJSONDecoder.load_ietf_json(
+        json.load(open(os.path.join(os.path.dirname(__file__), "json", "nonexist.json"), 'r')),
+        self.bindings,
+        "json_deserialise",
+        skip_unknown=True
+      )
     except AttributeError:
-      success = False
+      allowed = False
+    self.assertTrue(allowed, "Skipping keys that did not exist was not successfully handled.")
 
-    assert success is i, "Skipping keys that did not exist was not" + \
-      " successfully handled"
-
-  if not k:
-    os.system("/bin/rm %s/bindings.py" % this_dir)
-    os.system("/bin/rm %s/bindings.pyc" % this_dir)
+  def test_dont_skip_unknown_keys(self):
+    allowed = True
+    try:
+      pybindJSONDecoder.load_ietf_json(
+        json.load(open(os.path.join(os.path.dirname(__file__), "json", "nonexist.json"), 'r')),
+        self.bindings,
+        "json_deserialise",
+        skip_unknown=False
+      )
+    except AttributeError:
+      allowed = False
+    self.assertFalse(allowed, "Skipping keys that did not exist was not successfully handled.")
 
 
 if __name__ == '__main__':
-  main()
+  unittest.main()
