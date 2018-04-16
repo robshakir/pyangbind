870af8080a0b5e44403c9a2fe4cc78dc119e9498
Joey Wilhelm <tarkatronic@gmail.com>
2018-04-16 15:01:28 -0700

    Convert the uint tests to use unittest (#182)

--- tests/uint/run.py
+++ tests/uint/run.py
@@ -1,196 +1,119 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import getopt
+from __future__ import unicode_literals
 
-TESTNAME = "uint"
+import unittest
+
+from tests.base import PyangBindTestCase
 
 
-# generate bindings in this folder
-def main():
-  try:
-    opts, args = getopt.getopt(sys.argv[1:], "k", ["keepfiles"])
-  except getopt.GetoptError as e:
-    print(str(e))
-    sys.exit(127)
+class UIntTests(PyangBindTestCase):
+  yang_files = ['uint.yang']
 
-  k = False
-  for o, a in opts:
-    if o in ["-k", "--keepfiles"]:
-      k = True
+  def setUp(self):
+    self.instance = self.bindings.uint()
 
-  pythonpath = os.environ.get("PATH_TO_PYBIND_TEST_PYTHON") if \
-                os.environ.get('PATH_TO_PYBIND_TEST_PYTHON') is not None \
-                  else sys.executable
-  pyangpath = os.environ.get('PYANGPATH') if \
-                os.environ.get('PYANGPATH') is not None else False
-  pyangbindpath = os.environ.get('PYANGBINDPATH') if \
-                os.environ.get('PYANGBINDPATH') is not None else False
-  assert pyangpath is not False, "could not find path to pyang"
-  assert pyangbindpath is not False, "could not resolve pyangbind directory"
+  def test_uint_maximum_default_values(self):
+    for (size, value) in [("eight", 2**8 - 1), ("sixteen", 2**16 - 1), ("thirtytwo", 2**32 - 1),
+                          ("sixtyfour", 2**64 - 1)]:
+      with self.subTest(size=size, value=value):
+        default = getattr(self.instance.uint_container, "%sdefault" % size)._default
+        self.assertEqual(default, value)
 
-  this_dir = os.path.dirname(os.path.realpath(__file__))
+  def test_set_uint_values(self):
+    for leaf in ["eight", "sixteen", "thirtytwo", "sixtyfour"]:
+      with self.subTest(leaf=leaf):
+        setattr(self.instance.uint_container, leaf, 42)
+        value = getattr(self.instance.uint_container, leaf)
+        self.assertEqual(value, 42)
 
-  cmd = "%s " % pythonpath
-  cmd += "%s --plugindir %s/pyangbind/plugin" % (pyangpath, pyangbindpath)
-  cmd += " -f pybind -o %s/bindings.py" % this_dir
-  cmd += " -p %s" % this_dir
-  cmd += " %s/%s.yang" % (this_dir, TESTNAME)
-  os.system(cmd)
+  def test_set_uint_values_marks_changes(self):
+    for leaf in ["eight", "sixteen", "thirtytwo", "sixtyfour"]:
+      with self.subTest(leaf=leaf):
+        setattr(self.instance.uint_container, leaf, 42)
+        leaf_ref = getattr(self.instance.uint_container, leaf)
+        self.assertTrue(leaf_ref._changed())
 
-  from bindings import uint
+  def test_uint8_with_restricted_range(self):
+    for (value, valid) in [(0, False), (7, True), (11, False)]:
+      with self.subTest(value=value, valid=valid):
+        allowed = True
+        try:
+          self.instance.uint_container.eightrestricted = value
+        except ValueError:
+          allowed = False
+        self.assertEqual(allowed, valid)
 
-  u = uint()
+  def test_uint16_with_restricted_range(self):
+    for (value, valid) in [(99, False), (123, True), (1001, False)]:
+      with self.subTest(value=value, valid=valid):
+        allowed = True
+        try:
+          self.instance.uint_container.sixteenrestricted = value
+        except ValueError:
+          allowed = False
+        self.assertEqual(allowed, valid)
 
-  for name in ["eight", "sixteen", "thirtytwo", "sixtyfour"]:
-    for subname in ["", "default", "result", "restricted"]:
-      assert hasattr(u.uint_container, "%s%s" % (name, subname)) is True, \
-          "missing %s%s from container" % (name, subname)
+  def test_uint32_with_restricted_range(self):
+    for (value, valid) in [(9999, False), (424242, True), (500001, False)]:
+      with self.subTest(value=value, valid=valid):
+        allowed = True
+        try:
+          self.instance.uint_container.thirtytworestricted = value
+        except ValueError:
+          allowed = False
+        self.assertEqual(allowed, valid)
 
-  # tests default and equality
-  for e in [("eight", 2**8 - 1), ("sixteen", 2**16 - 1),
-            ("thirtytwo", 2**32 - 1), ("sixtyfour", 2**64 - 1)]:
-    default_v = getattr(u.uint_container, "%sdefault" % e[0])._default
-    assert default_v == e[1], \
-      "defaults incorrectly set for %s, expected: %d, got %d" \
-          % (e[0], e[1], default_v)
+  def test_uint64_with_restricted_range(self):
+    for (value, valid) in [(799, False), (42424242, True), (18446744073709551615, False)]:
+      with self.subTest(value=value, valid=valid):
+        allowed = True
+        try:
+          self.instance.uint_container.sixtyfourrestricted = value
+        except ValueError:
+          allowed = False
+        self.assertEqual(allowed, valid)
 
-  u.uint_container.eight = 42
-  u.uint_container.sixteen = 42
-  u.uint_container.thirtytwo = 42
-  u.uint_container.sixtyfour = 42
-  u.uint_container.eightdefault = 84
-  u.uint_container.sixteendefault = 84
-  u.uint_container.thirtytwodefault = 84
-  u.uint_container.sixtyfourdefault = 84
-  for i in ["eight", "sixteen", "thirtytwo", "sixtyfour"]:
-    v = getattr(u.uint_container, i)
-    assert v == 42, "incorrectly set %s, expected 42, got %d" % (i, v)
-    c = getattr(u.uint_container, "_changed")()
-    assert c is True, "incorrect changed flag for %s" % i
+  def test_additional_uint32_range(self):
+    for (value, valid) in [(0, True), (10, True), (2**32 - 1, True), (2**64, False)]:
+      with self.subTest(value=value, valid=valid):
+        allowed = True
+        try:
+          self.instance.issue_fixes.region_id = value
+        except ValueError:
+          allowed = False
+        self.assertEqual(allowed, valid)
 
-  # maybe we need to test math here
+  def test_set_uint_values_to_zero(self):
+    for size in ["eight", "sixteen", "thirtytwo", "sixtyfour"]:
+      with self.subTest(size=size):
+        setter = getattr(self.instance.uint_container, "_set_%s" % size)
+        allowed = True
+        try:
+          setter(0)
+        except ValueError:
+          allowed = False
+        self.assertTrue(allowed)
 
-  e = False
-  try:
-    u.uint_container.eightrestricted = 11
-  except ValueError:
-    e = True
-  assert e is True, \
-     "incorrectly allowed value outside of range for eightrestricted (11)"
+  def test_set_uint_values_below_zero(self):
+    for size in ["eight", "sixteen", "thirtytwo", "sixtyfour"]:
+      with self.subTest(size=size):
+        setter = getattr(self.instance.uint_container, "_set_%s" % size)
+        with self.assertRaises(ValueError):
+          setter(-1)
 
-  e = False
-  try:
-    u.uint_container.eightrestricted = 0
-  except ValueError:
-    e = True
-  assert e is True, \
-     "incorrectly allowed value outside of range for eightrestricted (0)"
-
-  e = False
-  try:
-    u.uint_container.sixteenrestricted = 1001
-  except ValueError:
-    e = True
-  assert e is True, \
-     "incorrectly allowed value outside of range for sixteenrestricted (1001)"
-
-  e = False
-  try:
-    u.uint_container.sixteenrestricted = 99
-  except ValueError:
-    e = True
-  assert e is True, \
-     "incorrectly allowed value outside of range for sixteenrestricted (99)"
-
-  e = False
-  try:
-    u.uint_container.thirtytworestricted = 500001
-  except ValueError:
-    e = True
-  assert e is True, \
-     "incorrectly allowed value outside of range for thirtytworestricted " + \
-        "(500001)"
-
-  e = False
-  try:
-    u.uint_container.thirtytworestricted = 9999
-  except ValueError:
-    e = True
-  assert e is True, \
-     "incorrectly allowed value outside of range for thirtytworestricted " + \
-          "(9999)"
-
-  e = False
-  try:
-    u.uint_container.sixtyfourrestricted = 18446744073709551615
-  except ValueError:
-    e = True
-  assert e is True, \
-     "incorrectly allowed value outside of range for sixtyfourrestricted " + \
-        "(18446744073709551615)"
-
-  e = False
-  try:
-    u.uint_container.sixtyfourrestricted = 799
-  except ValueError:
-    e = True
-  assert e is True, \
-     "incorrectly allowed value outside of range for sixtyfourrestricted (799)"
-
-  for v in [(0, True), (10, True), (2**32 - 1, True), (2**64, False),
-              (2**32, False)]:
-    val_set = False
-    try:
-      u.issue_fixes.region_id = v[0]
-      val_set = True
-    except ValueError:
-      pass
-    assert v[1] is val_set, "Value of region_id incorrectly set %s (%s != %s" \
-      % (v[0], val_set, v[1])
-
-  bounds = {
-    'eight': (0, 2**8 - 1),
-    'sixteen': (0, 2**16 - 1),
-    'thirtytwo': (0, 2**32 - 1),
-    'sixtyfour': (0, 2**64 - 1),
-  }
-
-  for elem, vals in bounds.iteritems():
-    set_attr = getattr(u.uint_container, "_set_%s" % elem, None)
-    assert set_attr is not None, "Could not find attribute"
-    for val in vals:
-      passed = True
-      try:
-        set_attr(vals[0])
-      except ValueError:
-        passed = False
-      assert passed is True, "Could not set int size %s to %d" % \
-        (elem, val)
-
-    passed = False
-    try:
-      set_attr(vals[0] - 1)
-    except ValueError:
-      passed = True
-
-    assert passed is True, "Incorrectly set int size %s to %d" % \
-      (elem, vals[0] - 1)
-
-    passed = False
-    try:
-      set_attr(vals[1] + 1)
-    except ValueError:
-      passed = True
-
-    assert passed is True, "Incorrectly set int size %s to %d" % \
-      (elem, vals[1] + 1)
-
-  if not k:
-    os.system("/bin/rm %s/bindings.py" % this_dir)
-    os.system("/bin/rm %s/bindings.pyc" % this_dir)
+  def test_set_uint_values_above_upper_bounds(self):
+    bounds = {
+      'eight': 2**8,
+      'sixteen': 2**16,
+      'thirtytwo': 2**32,
+      'sixtyfour': 2**64
+    }
+    for size, value in bounds.items():
+      with self.assertRaises(ValueError), self.subTest(size=size, value=value):
+        setter = getattr(self.instance.uint_container, "_set_%s" % size)
+        setter(value)
 
 
 if __name__ == '__main__':
-  main()
+  unittest.main()
