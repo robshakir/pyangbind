9f1997716e79ce81a2ef6ad091cb1f3db6532ae4
Rob Shakir <robjs@google.com>
2018-04-20 18:39:43 -0700

    Update scripts for release
      * (M) scripts/release.sh
        - Run coverage through tox rather than using run.sh
      * (M) setup.py
        - Fix typo.

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,8 @@ THISDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 rm $THISDIR/../dist/*.whl $THISDIR/../dist/*.tar.gz
 
 python $THISDIR/../setup.py bdist_wheel sdist
-$THISDIR/../tests/run.sh
+
+coverage run setup.py test -q
 
 if [ $? -ne 0 ]; then
     echo "FAILED: Cannot release a broken version!"
