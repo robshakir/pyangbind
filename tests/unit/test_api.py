451ac4430afb56bd9064db3b7f06e33354a8d110
David Barroso <dbarrosop@dravetech.com>
2018-06-03 21:56:25 +0200

    break down tests

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -5,18 +5,22 @@ class APITests(PyangBindTestCase):
     yang_files = ['models/simple.yang']
 
     def setUp(self):
-        self.simple = self.bindings.simple()
+        self.simple_unchanged = self.bindings.simple()
+        self.simple_changed_defaults = self.bindings.simple()
+        self.simple_changed_non_defaults = self.bindings.simple()
 
-    def test_leaf_changed(self):
-        assert not self.simple.test_leaf_changed.a1._changed()
-        assert not self.simple.test_leaf_changed._changed()
+    def test_leaf_unchanged(self):
+        self.assertFalse(self.simple.test_leaf_changed.a1._changed())
+        self.assertFalse(self.simple.test_leaf_changed._changed())
 
-        self.simple.test_leaf_changed.a1 = self.simple.test_leaf_changed.a1._default
-        assert self.simple.test_leaf_changed.a1 == self.simple.test_leaf_changed.a1._default
-        assert self.simple.test_leaf_changed.a1._changed()
-        assert self.simple.test_leaf_changed._changed()
+    def test_leaf_changed_with_defaults(self):
+        self.simple_changed_defaults.test_leaf_changed.a1 = self.simple.test_leaf_changed.a1._default
+        self.assertTrue(self.simple_changed_defaults.test_leaf_changed.a1 == self.simple_changed_defaults.test_leaf_changed.a1._default)
+        self.assertTrue(self.simple_changed_defaults.test_leaf_changed.a1._changed())
+        self.assertTrue(self.simple_changed_defaults.test_leaf_changed._changed())
 
-        self.simple.test_leaf_changed.a1 = "test"
-        assert self.simple.test_leaf_changed.a1 == "test"
-        assert self.simple.test_leaf_changed.a1._changed()
-        assert self.simple.test_leaf_changed._changed()
+    def test_leaf_changed_with_non_defaults(self):
+        self.simple_changed_non_defaults.test_leaf_changed.a1 = "test"
+        self.assertTrue(self.simple_changed_non_defaults.test_leaf_changed.a1 == "test")
+        self.assertTrue(self.simple_changed_non_defaults.test_leaf_changed.a1._changed())
+        self.assertTrue(self.simple_changed_non_defaults.test_leaf_changed._changed())
