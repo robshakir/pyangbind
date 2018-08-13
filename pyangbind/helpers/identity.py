df303b101dd42430632bf8b066beeddf3e61ef4f
guojunzhang <guojunzhang1981@gmail.com>
2018-08-13 09:38:05 -0700

    Fix missing namespace in identityref field
    When we generate python code from Yang module, the restriction_arg for
    the field doesn't include module name as the prefix.

--- pyangbind/helpers/identity.py
+++ pyangbind/helpers/identity.py
@@ -30,6 +30,8 @@ class Identity(object):
         self.children = []
 
     def add_prefix(self, prefix):
+        if self.source_module not in self._imported_prefixes:
+            self._imported_prefixes.append(self.source_module)
         if prefix not in self._imported_prefixes:
             self._imported_prefixes.append(prefix)
 
