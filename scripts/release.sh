7a8f828eeb55c73f2aa85cb76d0a4c12c4680fb1
Jose Ignacio Tamayo <jose.ignacio.tamayo@gmail.com>
2023-08-11 07:55:16 +0200

    Update Release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 THISDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BASEDIR=$THISDIR/..
+rm $BASEDIR/dist/*.whl $BASEDIR/dist/*.tar.gz
 
-rm $THISDIR/../dist/*.whl $THISDIR/../dist/*.tar.gz
+virtualenv $BASEDIR/pyvirtualenv
+source $BASEDIR/pyvirtualenv/bin/activate
 
-python $THISDIR/../setup.py bdist_wheel sdist
+pip install -r $BASEDIR/requirements.txt
+pip install tox
 
-coverage run setup.py test -q
-
+python -m tox --root $BASEDIR/
 if [ $? -ne 0 ]; then
     echo "FAILED: Cannot release a broken version!"
     exit 127
 fi
 
-twine upload -s $THISDIR/../dist/pyangbind*
+pip install twine build
+python -m build --outdir $BASEDIR/dist/
+
+python -m twine upload -s $BASEDIR/dist/pyangbind*
