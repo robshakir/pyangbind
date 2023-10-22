d289841129cade86195af4dbfe030511abd40bdc
Jose Ignacio Tamayo <jose.ignacio.tamayo@gmail.com>
2023-10-22 12:07:10 +0200

    Iterate on release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,13 +2,11 @@
 
 THISDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BASEDIR=$THISDIR/..
-rm $BASEDIR/dist/*.whl $BASEDIR/dist/*.tar.gz
 
-virtualenv $BASEDIR/pyvirtualenv
+virtualenv --clear $BASEDIR/pyvirtualenv
 source $BASEDIR/pyvirtualenv/bin/activate
 
-pip install -r $BASEDIR/requirements.txt
-pip install tox
+pip install -r $BASEDIR/requirements.txt -r $BASEDIR/requirements.DEVELOPER.txt
 
 python -m tox --root $BASEDIR/
 if [ $? -ne 0 ]; then
@@ -16,10 +14,10 @@ if [ $? -ne 0 ]; then
     exit 127
 fi
 
-pip install twine build
+rm -rf $BASEDIR/dist
 python -m build --outdir $BASEDIR/dist/ $BASEDIR/
-
 pip install --force-reinstall $BASEDIR/dist/pyangbind-*.whl
+
 export PYBINDPLUGIN=`/usr/bin/env python -c \
 'import pyangbind; import os; print ("{}/plugin".format(os.path.dirname(pyangbind.__file__)))'`
 if ! pyang -V --plugindir $PYBINDPLUGIN -f pybind -o $BASEDIR/tests/base-binding-out.py $BASEDIR/tests/base-test.yang; then
@@ -27,4 +25,7 @@ if ! pyang -V --plugindir $PYBINDPLUGIN -f pybind -o $BASEDIR/tests/base-binding
     exit 126
 fi
 
-python -m twine upload -s $BASEDIR/dist/pyangbind*
+pip install twine
+python -m twine upload  $BASEDIR/dist/pyangbind*
+
+deactivate
