4397fc723e1e34bc71ca9329ccc59383f41a1509
Jose Ignacio Tamayo <jose.ignacio.tamayo@gmail.com>
2024-02-01 15:29:50 +0100

    Add UnitTest for nested Choices

--- tests/choice/run.py
+++ tests/choice/run.py
@@ -15,7 +15,7 @@ class ChoicesTests(PyangBindTestCase):
         self.assertTrue(hasattr(self.choice_obj, "container"), "Object does not have container")
 
     def test_class_has_choice_containers(self):
-        for container in ["case_one_container", "case_two_container"]:
+        for container in ["case_one_container", "case_two_container", "case_three_one_container"]:
             with self.subTest(container=container):
                 self.assertTrue(
                     hasattr(self.choice_obj.container, container),
@@ -23,27 +23,36 @@ class ChoicesTests(PyangBindTestCase):
                 )
 
     def test_class_does_not_have_choices_as_attributes(self):
-        for choice in ["choice_one", "choice_two", "case_one", "case_two"]:
+        for choice in [
+            "choice_one",
+            "choice_three",
+            "case_one",
+            "case_two",
+            "case_three_one",
+            "case_three_two",
+        ]:
             with self.subTest(choice=choice):
                 self.assertFalse(
                     hasattr(self.choice_obj, choice), "Object has an erroneous choice option, %s" % choice
                 )
 
     def test_case_leaf_default_values(self):
-        for leaf in ["case_one", "case_two"]:
+        for leaf in ["case_one", "case_two", "case_three_one"]:
             with self.subTest(leaf=leaf):
                 container = getattr(self.choice_obj.container, "%s_container" % leaf)
                 value = getattr(container, "%s_leaf" % leaf)
                 self.assertEqual(value, 0, "Object does not have the correct value for %s_leaf, %s" % (leaf, value))
 
     def test_set_choice_value(self):
-        self.choice_obj.container.case_one_container.case_one_leaf = 42
-        self.assertEqual(
-            self.choice_obj.container.case_one_container.case_one_leaf,
-            42,
-            "Object did not specify a value within the choice correctly, %s"
-            % self.choice_obj.container.case_one_container.case_one_leaf,
-        )
+        for leaf in ["case_one", "case_two", "case_three_one"]:
+            with self.subTest(leaf=leaf):
+                container = getattr(self.choice_obj.container, "%s_container" % leaf)
+                value = setattr(container, "%s_leaf" % leaf, 42)
+                self.assertEqual(
+                    getattr(container, "%s_leaf" % leaf, 0),
+                    42,
+                    "Object did not specify a value within the choice correctly, %s" % container,
+                )
 
     def test_set_choice_value_doesnt_set_other_choices(self):
         self.choice_obj.container.case_one_container.case_one_leaf = 42
@@ -60,7 +69,7 @@ class ChoicesTests(PyangBindTestCase):
         self.assertEqual(
             self.choice_obj.container.case_two_container.case_two_leaf,
             42,
-            "Object did not allow the other half of the choice field to be specified, %s"
+            "Object did not allow the other choice field to be specified, %s"
             % self.choice_obj.container.case_two_container.case_two_leaf,
         )
 
@@ -99,6 +108,15 @@ class ChoicesTests(PyangBindTestCase):
             0,
             "Adding to the second user list did not remove entries from the first",
         )
+
+    def test_set_nested_choice(self):
+        for leaf in [
+            self.choice_obj.container.case_three_one_container.case_three_one_leaf,
+            self.choice_obj.container.case_three_two_leaf,
+        ]:
+            with self.subTest(leaf=leaf):
+                leaf = 99
+                self.assertEqual(leaf, 99, "Nested choice leaf not set")
 
 
 if __name__ == "__main__":
