edf08cd9fcbef6d1dcb5ec533d501edc01ce4828
Jose Ignacio Tamayo Segarra <jose.ignacio.tamayo@gmail.com>
2024-02-28 15:55:33 +0100

    Replace six.text_type for str in tests

--- tests/serialise/json-serialise/run.py
+++ tests/serialise/json-serialise/run.py
@@ -55,7 +55,7 @@ class JSONSerialiseTests(PyangBindTestCase):
         self.serialise_obj.c1.l1[1].typedef_two = 8
         self.serialise_obj.c1.l1[1].one_leaf = "hi"
         for i in range(1, 5):
-            self.serialise_obj.c1.l1[1].ll.append(six.text_type(i))
+            self.serialise_obj.c1.l1[1].ll.append(str(i))
         self.serialise_obj.c1.l1[1].next_hop.append("DROP")
         self.serialise_obj.c1.l1[1].next_hop.append("192.0.2.1")
         self.serialise_obj.c1.l1[1].next_hop.append("fish")
