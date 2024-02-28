d8748275ec2466cc51d99bd18c6adad0447a8ec8
Jose Ignacio Tamayo Segarra <jose.ignacio.tamayo@gmail.com>
2024-02-28 15:16:00 +0100

    Replace six.string_types

--- pyangbind/lib/yangtypes.py
+++ pyangbind/lib/yangtypes.py
@@ -297,7 +297,7 @@ def RestrictedClassType(*args, **kwargs):
 
             def match_pattern_check(regexp):
                 def mp_check(value):
-                    if not isinstance(value, six.string_types + (six.text_type,)):
+                    if not isinstance(value, (str,)):
                         return False
                     if regex.match(convert_regexp(regexp), value):
                         return True
@@ -456,11 +456,11 @@ def TypedListType(*args, **kwargs):
                             tmp = i(v)
                             passed = True
                             break
-                    elif i == six.text_type and isinstance(v, six.string_types + (six.text_type,)):
+                    elif i == str and isinstance(v, (str,)):
                         tmp = str(v)
                         passed = True
                         break
-                    elif i not in six.string_types + (six.text_type,):
+                    elif i not in (str,):
                         # for anything other than string we try
                         # and cast. Using things for string or
                         # unicode gives us strange results because we get
